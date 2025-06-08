44a06e949ebd51a1c6a446ae9ad7b50e7a64043b
jynnguyen <hieunguyen126@hcmut.edu.vn>
2025-06-08 22:51:08 +0700

    first commit

--- run.bat
+++ run.bat
@@ -1,7 +1,8 @@
 @echo off
 cd /d "%~dp0"
-
+cls
 REM ===== 1. Clean & Rebuild =====
+powershell -Command "Write-Host ' > Building project with CMake... ' -ForegroundColor Yellow"
 if exist build (
     rmdir /s /q build
 )
@@ -14,12 +15,13 @@ cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release ..
 echo [CMAKE] Building project...
 cmake --build .
 echo.
+powershell -Command "Write-Host ' >> Build completed.' -ForegroundColor Green"
+echo.
 
 cd ..
 
 REM ===== 2. Prepare Distribution Folder =====
 echo [PACKAGE] Preparing dist folder...
-
 if exist dist (
     rmdir /s /q dist
 )
@@ -30,7 +32,6 @@ mkdir dist\Data
 copy Data dist\Data\
 
 REM ===== 3. Copy Required DLLs (from MinGW bin folder) =====
-REM ⚠️ Update this path to your actual MinGW bin folder
 set MINGW_BIN="C:\msys64\ucrt64\bin"
 
 echo [PACKAGE] Copying DLLs...
@@ -44,6 +45,7 @@ echo [STRIP] Removing debug symbols...
 
 REM ===== 5. Done =====
 echo.
-powershell -Command "Write-Host 'Build and package completed. Files are in /dist' -ForegroundColor Green"
+powershell -Command "Write-Host ' >> Package completed.' -ForegroundColor Green"
+powershell -Command "Write-Host ' Files are in dist folder' -ForegroundColor Cyan"
 echo.
 pause
